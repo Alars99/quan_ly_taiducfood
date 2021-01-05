6c756c66bf43d32977f8c10567b6f2bfd7f415f2
Alars99 <hoanglong291099@gmail.com>
2021-01-05 22:38:09 +0700

    fix5/1

--- lib/main_action/home/my_diary_screen.dart
+++ lib/main_action/home/my_diary_screen.dart
@@ -48,25 +48,22 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
     referenceProduct.once().then((DataSnapshot snapshot) {
       _list.clear();
       var keys = snapshot.value.keys;
-      if (keys == null) {
-      } else {
-        var values = snapshot.value;
-        for (var key in keys) {
-          OrderList order = new OrderList(
-            values[key]["idDonHang"],
-            values[key]["idGioHang"],
-            values[key]["tongTienhang"],
-            values[key]["tongSoluong"],
-            values[key]["phiGiaohang"],
-            values[key]["chietKhau"],
-            values[key]["banSiLe"],
-            values[key]["paymethod"],
-            values[key]["idKhachHang"],
-            values[key]["ngaymua"],
-            values[key]["trangthai"],
-          );
-          _list.add(order);
-        }
+      var values = snapshot.value;
+      for (var key in keys) {
+        OrderList order = new OrderList(
+          values[key]["idDonHang"],
+          values[key]["idGioHang"],
+          values[key]["tongTienhang"],
+          values[key]["tongSoluong"],
+          values[key]["phiGiaohang"],
+          values[key]["chietKhau"],
+          values[key]["banSiLe"],
+          values[key]["paymethod"],
+          values[key]["idKhachHang"],
+          values[key]["ngaymua"],
+          values[key]["trangthai"],
+        );
+        _list.add(order);
       }
 
       for (var don in _list) {
